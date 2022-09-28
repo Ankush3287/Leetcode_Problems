6d72a39450f506dcf2a245b90ab13c1114901215
Ankush Solanki <76993439+Ankush3287@users.noreply.github.com>
2022-09-28 17:30:19 +0530

    Time: 14 ms (32.24%), Space: 8.9 MB (52.51%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,27 +1,20 @@
 class Solution {
-    public:
-    int f(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp){
-        int n=triangle.size();
-        if(i==(n-1))    return triangle[i][j];
-        if(dp[i][j]!=-1)    return dp[i][j];
-        int down=f(i+1,j,triangle,dp);
-        int diagonal=f(i+1,j+1,triangle,dp);
-        return dp[i][j]=triangle[i][j]+min(down,diagonal);
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        vector<int>prev(n,0);
         for(int i=0;i<n;i++){
-            dp[n-1][i]=triangle[n-1][i];
+            prev[i]=triangle[n-1][i];
         }
         for(int i=n-2;i>=0;i--){
+            vector<int>curr(n,0);
             for(int j=0;j<=i;j++){
-                int down=dp[i+1][j];
-                int diagonal=dp[i+1][j+1];
-                dp[i][j]=triangle[i][j]+min(down,diagonal);
+                int down=prev[j];
+                int diagonal=prev[j+1];
+                curr[j]=triangle[i][j]+min(down,diagonal);
             }
+            prev=curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
