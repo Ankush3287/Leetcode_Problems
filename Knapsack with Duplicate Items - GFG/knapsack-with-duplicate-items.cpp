47d3782b5526233a8a22792b95db8d27fbe5eacd
Ankush Solanki <76993439+Ankush3287@users.noreply.github.com>
2022-10-12 18:10:17 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -8,23 +8,11 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-    private:
-    int f(int ind,int W,int val[],int wt[],vector<vector<int>>&dp){
-        if(ind ==0){
-              return val[0]*(W/wt[0]);
-        }
-        if(dp[ind][W]!=-1)  return dp[ind][W];
-        int pick=-1e9;
-        int notPick=f(ind-1,W,val,wt,dp );
-        if(wt[ind]<=W)  pick=val[ind]+f(ind,W-wt[ind],val,wt,dp);
-        return dp[ind][W]=max(pick,notPick);
-    }
 public:
     int knapSack(int N, int W, int val[], int wt[])
     {
         // code here
-        vector<vector<int>>dp(N,vector<int>(W+1,0));
-        vector<int>prev(W+1),curr(W+1,0);
+        vector<int>prev(W+1);
         for(int i=0;i<=W;i++){
             prev[i]=val[0]*(i/wt[0]);
         }
@@ -32,10 +20,10 @@ public:
             for(int j=0;j<=W;j++){
                 int pick=0;
                 int notPick=prev[j];
-                if(wt[i]<=j)  pick=val[i]+curr[j-wt[i]];
-                curr[j]=max(pick,notPick);
+                if(wt[i]<=j)  pick=val[i]+prev[j-wt[i]];
+                prev[j]=max(pick,notPick);
             }
-            prev=curr;
+           
         }
         return prev[W];
         
