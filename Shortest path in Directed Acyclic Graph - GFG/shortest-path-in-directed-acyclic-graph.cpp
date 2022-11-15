dacf903c14214c5dadff8f74601beb4d67993059
Ankush Solanki <76993439+Ankush3287@users.noreply.github.com>
2022-11-15 22:06:49 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -8,10 +8,12 @@ using namespace std;
 // User function Template for C++
 class Solution {
     private:
-    void dfs(int node,vector<pair<int,int>> adj[],stack<int> &st,vector<int> &vis){
+    void dfs(vector<pair<int,int>>adj[],int node,vector<int>&vis,stack<int>&st){
         vis[node]=1;
         for(auto it:adj[node]){
-            if(!vis[it.first])  dfs(it.first,adj,st,vis);
+            if(!vis[it.first]){
+                dfs(adj,it.first,vis,st);
+            }
         }
         st.push(node);
     }
@@ -20,17 +22,16 @@ class Solution {
         // code here
         vector<pair<int,int>> adj[N];
         for(int i=0;i<M;i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            int w=edges[i][2];
-            adj[u].push_back({v,w});
+            adj[edges[i][0]].push_back({edges[i][1],edges[i][2]});
         }
-        stack<int> st;
         vector<int>vis(N,0);
+        stack<int>st;
         for(int i=0;i<N;i++){
-            if(!vis[i]) dfs(i,adj,st,vis);
+            if(!vis[i]){
+                dfs(adj,i,vis,st);
+            }
         }
-        vector<int> dist(N,1e9);
+        vector<int>dist(N,1e9);
         dist[0]=0;
         while(!st.empty()){
             int node=st.top();
@@ -41,7 +42,9 @@ class Solution {
             }
         }
         for(int i=0;i<N;i++){
-            if(dist[i]==1e9)    dist[i]=-1;
+            if(dist[i]==1e9){
+                dist[i]=-1;
+            }
         }
         return dist;
     }
