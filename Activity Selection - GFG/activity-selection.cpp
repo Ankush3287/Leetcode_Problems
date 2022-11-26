89447f323c41f1fe030a1fc58dd53dedeb002dbe
Ankush Solanki <76993439+Ankush3287@users.noreply.github.com>
2022-11-26 12:07:54 +0530

    Added solution - LeetHub

---
+++ Activity Selection - GFG/activity-selection.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of activities that can
+    //be performed by a single person.
+    int activitySelection(vector<int> start, vector<int> end, int n)
+    {
+        // Your code here
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        for(int i=0;i<n;i++){
+            pq.push({end[i],start[i]});
+        }
+        auto it=pq.top();
+        pq.pop();
+        int st=it.second;
+        int en=it.first;
+        int cnt=1;
+        while(!pq.empty()){
+            auto it=pq.top();
+            pq.pop();
+            int begin=it.second;
+            int finish=it.first;
+            if(begin>en){
+                cnt++;
+                st=begin;
+                en=finish;
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    
+    //testcases
+    cin >> t;
+    while(t--)
+    {
+        //size of array
+        int n;
+        cin >> n;
+        vector<int> start(n), end(n);
+        
+        //adding elements to arrays start and end
+        for(int i=0;i<n;i++)
+            cin>>start[i];
+        for(int i=0;i<n;i++)
+            cin>>end[i];
+        Solution ob;
+        //function call
+        cout << ob.activitySelection(start, end, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
